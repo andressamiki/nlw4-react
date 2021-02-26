2e5bc5ad6ace9cd901ef9dc04589eff78b41e7be
Andressa Miki <andressamiki@gmail.com>
2021-02-25 21:54:08 -0300

    Dia 1 - Entendendo estado, propriedade e componente

---
+++ src/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react'; // core da biblioteca
+import ReactDOM from 'react-dom'; // porque é usado na web
+import App from './App';
+
+ReactDOM.render(
+    <App />,//jsx : usar html dentro do js
+  document.getElementById('root')
+);
